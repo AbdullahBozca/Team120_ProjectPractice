ece8653551021df66efdba4b1d7681d544819bee
Bozca <abdullahbozca@gmail.co>
2023-08-19 21:44:54 +0300

    8/19/2023-2

--- src/agustos19_01/Dikdortgen.java
+++ src/agustos19_01/Dikdortgen.java
@@ -4,4 +4,9 @@ public class Dikdortgen extends Sekil{
     public Dikdortgen(double uzunKenar, double kisaKenar) {
         super(uzunKenar, kisaKenar);
     }
+
+    @Override
+    public String toString() {
+        return "uzunKenar=" + uzunKenar +", kisaKenar=" + kisaKenar + " çevresi=" + cevreHesaplama(uzunKenar,kisaKenar)+", alanı= "+alanHesapla(uzunKenar,kisaKenar);
+    }
 }
--- src/agustos19_01/Kare.java
+++ src/agustos19_01/Kare.java
@@ -1,7 +1,12 @@
 package agustos19_01;
 
-public class Kare extends Dikdortgen{
+public class Kare extends Dikdortgen {
     public Kare(double uzunKenar, double kisaKenar) {
         super(uzunKenar, kisaKenar);
+    }
+
+    @Override
+    public String toString() {
+        return "Kenar Uzunluğu=" + uzunKenar + " Çevre= "+cevreHesaplama(uzunKenar,kisaKenar)+" Alan= "+alanHesapla(uzunKenar,kisaKenar);
     }
 }
