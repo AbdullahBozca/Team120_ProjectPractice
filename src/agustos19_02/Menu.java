d118eed0a5e095c68c288f689bae7c841ce3313b
Bozca <abdullahbozca@gmail.co>
2023-08-21 19:05:25 +0300

    8/21/2023-1

---
+++ src/agustos19_02/Menu.java
@@ -0,0 +1,62 @@
+package agustos19_02;
+
+import java.util.Scanner;
+
+public class Menu extends Metodlar{
+    Scanner scan=new Scanner(System.in);
+    void giris(){
+        System.out.println("YENİ KULLANICI OLUŞTURMAK İÇİN '1' \n" +
+                "SİSTEME GİRİŞ İÇİN '2' YE BASINIZ...");
+        int secim=scan.nextInt();
+        switch (secim){
+            case 1:
+                kullaniciOlustur();
+                giris();
+            case 2:
+                if (kullaniciGirisi())
+                    menu();
+                else{
+                    System.out.println("HATALI GİRİŞ YAPTINIZ...");
+                    giris();
+                }
+
+
+            default:
+                System.out.println("HATALI GİRİŞ YAPTINIZ...");
+                giris();
+        }
+    }
+
+    private void menu() {
+        //Menu listesinde Bakiye sorgula, para yatirma, para çekme, para gönderme, sifre değiştirme ve cikis islemleri olacaktır.
+        System.out.print("YAPMAK İSTEDİĞİNİZ İŞLEMİ SEÇİNİZ\n" +
+                "1. BAKİYE SORGULAMA\n" +
+                "2. PARA YATIRMA\n" +
+                "3. PARA ÇEKME\n" +
+                "4. PARA GÖNDERME\n" +
+                "5. ŞİFRE DEĞİŞTİRME\n" +
+                "6. ÇIKIŞ\n" +
+                "SEÇİMİNİZ: ");
+        int secim=scan.nextInt();
+        switch (secim){
+            case 1:
+                bakiyeSorgula();
+                menu();
+            case 2:
+                paraYatirma();
+                menu();
+            case 3:
+                paraCekme();
+                menu();
+            case 4:
+                paraGonderme();
+                menu();
+            case 5:
+            case 6:
+            default:{
+                System.out.println("HATALI GİRİŞ YAPTINIZ...");
+                menu();
+            }
+        }
+    }
+}
